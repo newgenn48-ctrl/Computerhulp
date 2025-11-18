7c57813cef6a354028a02f6bea323970a3c7d217
Adam <hulpmetit57@gmail.com>
2025-11-18 14:40:02 +0100

    Fix build: Remove experimental optimizeCss (requires critters dependency)

--- next.config.js
+++ next.config.js
@@ -10,7 +10,6 @@ const nextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
   experimental: {
-    optimizeCss: true,
     optimizePackageImports: ['@/components'],
   },
   compiler: {
